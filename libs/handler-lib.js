f803ee98f52426c0c375060e0fbc56a592474b05
Umut Zafer <umutzafer@Umuts-MacBook-Pro.local>
2020-07-18 18:10:29 +0300

    not create api implemented

---
+++ libs/handler-lib.js
@@ -0,0 +1,22 @@
+export default function handler(lambda) {
+    return async function (event, context){
+        let body, statusCode;
+
+        try {
+            body = await lambda(event, context);
+            statusCode = 200;
+        } catch (e) {
+            body = e;
+            statusCode = 500;
+        }
+
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin" : "*",
+                "Access-Control-Allow-Credentials": true,
+            }
+        };
+    };
+}
