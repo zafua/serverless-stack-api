e79b74319daa5584196dec2fcf4c0006b4eed820
Umut Zafer <umutzafer@Umuts-MacBook-Pro.local>
2020-07-18 19:29:57 +0300

    not list api added

---
+++ list.js
@@ -0,0 +1,16 @@
+import handler from "./libs/handler-lib";
+import db from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId
+        }
+    };
+
+    const result = await db.query(params);
+
+    return result.Items;
+});
