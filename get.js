99dcc7502a168e6383262758e59a7203e9b3093d
Umut Zafer <umutzafer@Umuts-MacBook-Pro.local>
2020-07-25 19:56:19 +0300

    hata alacak deneme

--- get.js
+++ get.js
@@ -5,8 +5,7 @@ export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         Key: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: event.pathParameters.id
+            userId: event.requestContext.identity.cognitoIdentityId
         }
     };
 
